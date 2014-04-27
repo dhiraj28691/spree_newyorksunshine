97daf8decb12e4baca5c3bad4a87a7b3f1d19885
Michael Ray <michaelburtonray@gmail.com>
2014-04-26 22:31:42 -0400

    stockists

--- app/controllers/spree/stockists_controller.rb
+++ app/controllers/spree/stockists_controller.rb
@@ -6,7 +6,53 @@ module Spree
       @stockists = []
 
       @stockists << {
-        "name" => "Colette in Paris",
+        "name" => "Blue & Cream East Hampton",
+        "address" => "60 The Cir, East Hampton, NY 11937",
+        "phone_number" => "+1 631 329 1392",
+        "anchor" => "www.blueandcream.com",
+        "anchor_href" => "http://www.blueandcream.com/"
+      }
+
+      @stockists << {
+        "name" => "Reed Space",
+        "address" => "151 Orchard St, New York, NY 10002",
+        "phone_number" => "+1 212 253 0588",
+        "anchor" => "thereedspace.com",
+        "anchor_href" => "http://thereedspace.com/"
+      }
+
+      @stockists << {
+        "name" => "ODIN NY",
+        "address" => "199 Lafayette St, New York, NY 10012",
+        "phone_number" => "+1 212 966 0026",
+        "anchor" => "odinnewyork.com",
+        "anchor_href" => "http://odinnewyork.com/"
+      }
+
+      @stockists << {
+        "name" => "W. South Beach",
+        "address" => "2201 Collins Avenue, Miami Beach, Florida, 33139",
+        "phone_number" => "+1 305 938 3000"
+      }
+
+      @stockists << {
+        "name" => "Aloha Sunday Supply Company",
+        "address" => "3039 University Ave, San Diego, CA 92104, U.S.A.",
+        "phone_number" => "+1 619.269.9838",
+        "anchor" => "alohasunday.com",
+        "anchor_href" => "http://alohasunday.com/"
+      }
+
+      # @stockists << {
+      #   "name" => "Oakland Surf Club",
+      #   "address" => "337 14th St, Oakland, CA 94612",
+      #   "phone_number" => "+1 510 465 2770",
+      #   "anchor" => "oaklandsurfclub.com",
+      #   "anchor_href" => "http://oaklandsurfclub.com/"
+      # }
+
+      @stockists << {
+        "name" => "Colette",
         "address" => "213 Rue Saint Honoré, 75001 Paris, France",
         "phone_number" => "+33 1 55 35 33 90",
         "anchor" => "www.colette.fr",
@@ -14,7 +60,7 @@ module Spree
       }
 
       @stockists << {
-        "name" => "WISM in Tokyo",
+        "name" => "WISM",
         "address" => "5-17-20 Jingumae, Shibuya-Ku, 150-0001, Tokyo",
         "phone_number" => "+3 6418 5034",
         "anchor" => "wism-tyo.jp",
@@ -30,57 +76,10 @@ module Spree
       }
 
       @stockists << {
-        "name" => "ODIN NY",
-        "address" => "199 Lafayette St, New York, NY 10012",
-        "phone_number" => "+1 212 966 0026",
-        "anchor" => "odinnewyork.com",
-        "anchor_href" => "http://odinnewyork.com/"
-      }
-
-      @stockists << {
         "name" => "Fred Segal Japan",
         "anchor" => "www.fredsegal.jp",
         "anchor_href" => "http://www.fredsegal.jp/"
       }
-
-      @stockists << {
-        "name" => "W. South Beach",
-        "address" => "2201 Collins Avenue, Miami Beach, Florida, 33139",
-        "phone_number" => "+1 305 938 3000"
-      }
-
-      @stockists << {
-        "name" => "Blue & Cream East Hampton",
-        "address" => "60 The Cir, East Hampton, NY 11937",
-        "phone_number" => "+1 631 329 1392",
-        "anchor" => "www.blueandcream.com",
-        "anchor_href" => "http://www.blueandcream.com/"
-      }
-
-      @stockists << {
-        "name" => "Aloha Sunday Supply Company",
-        "address" => "3039 University Ave, San Diego, CA 92104, U.S.A.",
-        "phone_number" => "+1 619.269.9838",
-        "anchor" => "alohasunday.com",
-        "anchor_href" => "http://alohasunday.com/"
-      }
-
-      @stockists << {
-        "name" => "Reed Space",
-        "address" => "151 Orchard St, New York, NY 10002",
-        "phone_number" => "+1 212 253 0588",
-        "anchor" => "thereedspace.com",
-        "anchor_href" => "http://thereedspace.com/"
-      }
-
-      # @stockists << {
-      #   "name" => "Oakland Surf Club",
-      #   "address" => "337 14th St, Oakland, CA 94612",
-      #   "phone_number" => "+1 510 465 2770",
-      #   "anchor" => "oaklandsurfclub.com",
-      #   "anchor_href" => "http://oaklandsurfclub.com/"
-      # }
-
 
     end
 

46e99fccc242c0483f9c9e235ec39304fb79ffbd
Michael Ray <michaelburtonray@gmail.com>
2014-05-12 12:52:15 -0400

    new page has more images

--- app/controllers/spree/news_controller.rb
+++ app/controllers/spree/news_controller.rb
@@ -12,6 +12,13 @@ module Spree
       #   "images" => ["//files.human-nyc.com/newyorksunshine/assets/news-post1-image1.jpg"],
       #   "body_text" => "<p>Mastermind, the 6th LP from the 38 year old founder of Maybach Music Group or MMG for short, is a truly remarkable rap album. You usually expect to see artists that have grown at such a fast pace to sort of lose steam after their fourth or fifth album. Although, nothing seems to be slowing down the Rozay train. By surrounding himself with talent (Wale, Meek Mill, and French Montana just to name a few) he&#8217;s pushed not only his artists but himself to stay on track to go down as one of raps all-time greats.</p><p>The album is great because of two c’s, consistency and collaborations. Rick Ross has built so much of his career collaborating with the right people at the right time. This album is just another example of why rappers need to work with one another to obtain and maintain eminence. The consistency comes into play throughout the individual tracks. Although not every album is flawless, I would be lying if I said I only liked the chosen few singles on the album. &ldquo;The Devil is a Lie,&rdquo; smash radio hit. &ldquo;Black and White,&rdquo; that’s one you have to listen too with the top down on full blast. &ldquo;In Vein&rdquo; featuring The Weekend, a slowed down beat with not only a great chorus but arguably my favorite verse from Ricky on the whole album. And my personal favorite, &ldquo;Sanctified&rdquo; featuring Kanye West and Big Sean.</p><p>All in all, I plan on playing this LP all summer and even most likely well into the winter. If you haven’t picked it up, go out and grab a copy. If you don’t know, now you know&hellip;</p>"
       # }
+      @posts << {
+        "title" => "New York Sunshine Pop Up in WISM",
+        "date" => "05-12-2014",
+        "body_text" => "Here&lsquo;s our good friend, the owner of WISM in Tokyo, Japan, wearing our limited edition Hampton House Shirt in front of his store.",
+        "images" => ["//newyorksunshine-production.s3.amazonaws.com/news/hamptons-house-wism.jpg"]
+      }
+
 
       #2
       @posts << {
@@ -19,7 +26,7 @@ module Spree
         "date" => "04-22-2014",
         "embed_code" => '<iframe src="//player.vimeo.com/video/14074949" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
         "clickthrough_link" => "http://vimeo.com/14074949",
-        "body_text" => "No matter how many times we watch this video, we never cease to be amazed.  Gives us the chills every time&hellip;"
+        "body_text" => "No matter how many times we watch this video, we never cease to be amazed. Gives us the chills every time&hellip;"
       }
 
       #3
@@ -35,6 +42,7 @@ module Spree
       @posts << {
         "title" => "Sunshine State of Mind",
         "date" => "05-01-2014",
+        "images" => ["//newyorksunshine-production.s3.amazonaws.com/news/sunshine-state-of-mind.jpg"],
         "clickthrough_link" => "http://files.human-nyc.com/newyorksunshine/assets/news-manhattan-magazine-2014-05.pdf",
         "tags" => ["press"]
       }
@@ -43,7 +51,7 @@ module Spree
       @posts << {
         "title" => "Exploding From The Beach To The City",
         "date" => "07-24-2012",
-        "images" => ["http://files.human-nyc.com/newyorksunshine/assets/news-elitedaily.jpg"],
+        "images" => ["//newyorksunshine-production.s3.amazonaws.com/news/news-elitedaily.jpg"],
         "clickthrough_link" => "http://elitedaily.com/money/entrepreneurship/sunshine/",
         "tags" => ["press"]
       }
@@ -60,7 +68,7 @@ module Spree
       @posts << {
         "title" => "Swell Attire",
         "date" => "02-02-2014",
-        "images" => ["http://files.human-nyc.com/newyorksunshine/assets/news-swell-attire.jpg"],
+        "images" => ["//newyorksunshine-production.s3.amazonaws.com/news/news-swell-attire.jpg"],
         "clickthrough_link" => "http://www.insidehook.com/new-york/summer-surf-style/",
         "tags" => ["press"]
       }
@@ -70,7 +78,8 @@ module Spree
       @posts << {
         "title" => "The Sunshine Kid",
         "date" => "07-01-2013",
-        "clickthrough_link" => "http://files.human-nyc.com/newyorksunshine/assets/news-the-sunshine-kid.jpg",
+        "images" => ["//newyorksunshine-production.s3.amazonaws.com/news/beach-magazine-sunshine-kid-small.jpg"],
+        "clickthrough_link" => "//newyorksunshine-production.s3.amazonaws.com/news/beach-magazine-sunshine-kid.pdf",
         "tags" => ["press"]
       }
 
@@ -78,7 +87,8 @@ module Spree
       @posts << {
         "title" => "Summer In December",
         "date" => "06-01-3013",
-        "clickthrough_link" => "http://files.human-nyc.com/newyorksunshine/assets/news-new-york-post.pdf",
+        "images" => ["//newyorksunshine-production.s3.amazonaws.com/news/new-york-post-2013-small.jpg"],
+        "clickthrough_link" => "//newyorksunshine-production.s3.amazonaws.com/news/new-york-post-2013.pdf",
         "tags" => ["press"]
       }
 

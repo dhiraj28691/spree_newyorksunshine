bc5ddf27ffafd4f9838bebd1ba56725d97efc1ee
Michael Ray <michaelburtonray@gmail.com>
2014-09-13 23:42:08 -0400

    minor ui tweaks

--- app/overrides/add_content_to_admin_tabs.rb
+++ app/overrides/add_content_to_admin_tabs.rb
@@ -2,4 +2,4 @@ Deface::Override.new(
   :virtual_path => 'spree/admin/shared/_tabs',
   :name => 'add_content_to_admin_tabs_0',
   :insert_after => "erb[loud]:contains('tab :products')",
-  :text => "<%= tab :static, :taxonomies, :label => 'Content', :icon => 'file-text', :url => '/admin/posts/' %>")
+  :text => "<%= tab :home, :surf_club, :lookbook, :posts, :stockists, :about, :returns_and_exchanges, :label => 'Content', :icon => 'file-text', :url => '/admin/posts/' %>")

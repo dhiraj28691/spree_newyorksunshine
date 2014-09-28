0351dcb7d6e28fb769ddd556fedf90c2380ebf67
Michael Ray <michaelburtonray@gmail.com>
2014-09-28 18:18:21 -0400

    fixing migrations hopefully

--- db/migrate/20140404042642_create_spree_lookbooks.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from spree_newyorksunshine (originally )
-class CreateSpreeLookbooks < ActiveRecord::Migration
-  def change
-    create_table :spree_lookbooks do |t|
-
-      t.timestamps
-    end
-  end
-end
---
+++ db/migrate/20140914165307_create_spree_lookbooks.rb
@@ -0,0 +1,21 @@
+class AddNameAndSlugAndContentAndAvailableOnAndPublishedToSpreeLookbook < ActiveRecord::Migration
+  def change
+    # add_column :spree_lookbooks, :name, :string
+    # add_column :spree_lookbooks, :slug, :string
+    # add_index :spree_lookbooks, :slug, unique: true
+    # add_column :spree_lookbooks, :content, :text
+    # add_column :spree_lookbooks, :available_on, :datetime
+    # add_column :spree_lookbooks, :published, :boolean, default: true
+
+    create_table :spree_lookbooks do |t|
+      t.string :name
+      t.string :slug, index: true, unique: true
+      t.text :content
+      t.datetime :available_on
+      t.boolean :published, default: true
+
+      t.timestamps
+    end
+
+  end
+end

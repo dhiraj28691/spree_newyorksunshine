d71770241fb93814615f2dfba6f24de0c4af9faa
Michael Ray <michaelburtonray@gmail.com>
2014-09-29 23:05:37 -0400

    werddddddd

--- app/models/spree/slide_image.rb
+++ app/models/spree/slide_image.rb
@@ -6,7 +6,7 @@ class Spree::SlideImage < ActiveRecord::Base
   # has_attached_file :attachment,
   # :styles => { :landscape => "1920x1080#", :portrait => "960x1080#", :thumb => "100x100>" }
   has_attached_file :attachment,
-  :styles => { :full => "2560x1440>" }
+  :styles => { :full => "2560x1440>", :thumb => "100x100>" }
 
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
   validates :attachment, presence: true

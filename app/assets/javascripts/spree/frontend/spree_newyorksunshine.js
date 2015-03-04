b51124da166b526692e94711c04882664aee5c2a
Michael Ray <michaelburtonray@gmail.com>
2015-03-04 16:46:23 -0500

    frontend stuffs

--- app/assets/javascripts/spree/frontend/spree_newyorksunshine.js
+++ app/assets/javascripts/spree/frontend/spree_newyorksunshine.js
@@ -2,3 +2,27 @@
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
 //= require_tree .
+//= require util/router
+// var feed = new Feed(),
+//   slides = new Slides(),
+//   posts = new Posts();
+
+var art = new Art();
+
+
+
+
+var routes = [];
+// routes.push({ reg_exp: /^\/admin\/feed_items\/new$/, callback: feed.new });
+// routes.push({ reg_exp: /^\/admin\/feed_items\/([\w-]+)\/edit$/, callback: feed.edit });
+// routes.push({ reg_exp: /^\/admin\/lookbooks\/([\w-]+)\/slides\/new$/, callback: slides.form });
+// routes.push({ reg_exp: /^\/admin\/lookbooks\/([\w-]+)\/slides\/([\w-]+)\/edit$/, callback: slides.form });
+// routes.push({ reg_exp: /^\/admin\/posts\/new$/, callback: posts.form });
+// routes.push({ reg_exp: /^\/admin\/posts\/([\w-]+)\/edit$/, callback: posts.form });
+routes.push({ reg_exp: /^\/art$/, callback: art.index })
+
+
+var callback = new Router(routes);
+
+// route.init();
+document.addEventListener('DOMContentLoaded', callback);
--- app/assets/javascripts/util/router.js
+++ app/assets/javascripts/util/router.js
@@ -2,18 +2,22 @@ function Router(routes) {
   var callback = function(){};
 
   if(!routes.some(yas)) {
-   callback = function() { console.log('no matches'); }
+   callback = noMatches
   }
 
 
-  function yas(element) {
-    if(element.reg_exp.test(window.location.pathname)) {
-      callback = element.callback;
+  function yas(current_value, index, array) {
+    if(current_value.reg_exp.test(window.location.pathname)) {
+      callback = current_value.callback;
       // console.log(callback);
       return true;
     } else {
       return false;
     }
+  }
+
+  function noMatches() {
+    console.log('no matches');
   }
 
   // Router.prototype.init = callback

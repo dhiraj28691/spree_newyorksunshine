b52f5e300ad5ffd413e2c49ac027b26da5259c82
Michael Ray <michaelburtonray@gmail.com>
2015-03-06 21:19:22 -0500

    new lookbook js

---
+++ app/assets/javascripts/spree/frontend/lookbook.js
@@ -0,0 +1,49 @@
+function Lookbooks() {
+  "use strict";
+
+  var flkty;
+
+  Lookbooks.prototype.show = function(){
+
+
+
+    // Slideshow
+    flkty = new Flickity('#lookbook', {
+      cellAlign: 'center',
+      cellSelector: '.slide',
+      contain: true,
+      imagesLoaded: true,
+      // initialIndex: 2,
+      pageDots: false,
+      prevNextButtons: false,
+      wrapAround: true
+    });
+
+
+    var thumbnails_wrapper = document.querySelector('#thumbnails');
+    flkty.on( 'cellSelect', function() {
+      thumbnails_wrapper.querySelector('.current').classList.remove('current');
+      thumbnails_wrapper.querySelectorAll('.thumbnail').item(flkty.selectedIndex).classList.add('current');
+    });
+
+
+
+
+   document.querySelector('.left.capture').addEventListener('click', function(event){
+      flkty.previous();
+    });
+
+    document.querySelector('.right.capture').addEventListener('click', function(event){
+      flkty.next();
+    });
+
+    document.querySelector('#thumbnails').addEventListener('click', function(event){
+      event.preventDefault();
+      flkty.select($(event.target.parentNode).index());
+    }, true);
+
+
+
+
+  };
+}

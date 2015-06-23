d0e06bd0dcf0ad0b68587e5271ec4b47766d24eb
Michael Ray <michaelburtonray@gmail.com>
2015-06-23 08:01:49 -0400

    new backend changes

--- app/overrides/add_content_to_admin_tabs.rb
+++ app/overrides/add_content_to_admin_tabs.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  :virtual_path => 'spree/admin/shared/_tabs',
-  :name => 'add_content_to_admin_tabs_0',
-  :original => 'd2490102be74a6257e1e77b570c5e09f17d81cf7',
-  :insert_after => "erb[loud]:contains('tab *Spree::BackendConfiguration::PRODUCT_TABS')",
-  :text => "<%= tab :feed_items, :surf_club_images, :lookbooks, :slides, :slide_images, :posts, :stockists, :about, :returns_exchanges, :contents, :label => 'Content', :icon => 'file-text', :url => '/admin/feed_items' %>")
+  virtual_path: 'spree/layouts/admin',
+  insert_top: "[data-hook='admin_tabs']",
+  text: "<ul class=\"nav nav-sidebar\"><%= main_menu_tree \"Content\", icon: \"file\", sub_menu: \"content\", url: \"#sidebar-content\" %></ul>",
+  name: 'add_content_to_admin_tabs'
+)

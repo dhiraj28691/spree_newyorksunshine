ae60784f1a7785e7bddc2f58cac05010bebc3472
Michael Ray <michaelburtonray@gmail.com>
2015-07-02 19:25:35 -0400

    redefining image sizes

--- app/models/spree/feed_item.rb
+++ app/models/spree/feed_item.rb
@@ -6,7 +6,7 @@ class Spree::FeedItem < ActiveRecord::Base
 
   enum category: [ "Product", "Lookbook", "Post", "Other" ]
 
-  has_attached_file :attachment, :styles => { :cropped => "1080x1620#", :thumb => "100x150#" }
+  has_attached_file :attachment, :styles => { :cropped => "1080x1620>", :thumb => "100x150#" }
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 
   default_scope { order('position') }

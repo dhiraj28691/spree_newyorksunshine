952274216fb9ed5970c4bbcb365896679b08f406
Michael Ray <michaelburtonray@gmail.com>
2015-07-14 20:12:13 -0400

    remove image attachment

--- app/models/spree/feed_item.rb
+++ app/models/spree/feed_item.rb
@@ -1,4 +1,5 @@
 class Spree::FeedItem < ActiveRecord::Base
+  before_save :destroy_image?
 
   belongs_to :product
   belongs_to :lookbook
@@ -6,9 +7,22 @@ class Spree::FeedItem < ActiveRecord::Base
 
   enum category: [ "Product", "Lookbook", "Post", "Other" ]
 
-  has_attached_file :attachment, :styles => { :cropped => "1080x1620>", :thumb => "100x150#" }
+  # has_attached_file :attachment, :styles => { :cropped => "1080x1620>", :thumb => "100x150#" }
+  has_attached_file :attachment, :styles => { :cropped => "856x1248>", :thumb => "100x150#" }
+
+  attr_accessor :delete_attachment
+  before_validation { attachment.clear if delete_attachment == '1' }
+
+
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 
   default_scope { order('position') }
+
+
+  private
+
+  def destroy_image?
+    self.image.clear if @image_delete == "1"
+  end
 
 end

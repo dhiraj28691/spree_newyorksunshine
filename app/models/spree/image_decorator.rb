b2448dc22a891fbbb2f22e341f17283190f87e9b
aaronzdavis <aaronzdavis@gmail.com>
2017-02-09 11:50:50 -0800

    Update product feed for college sweatshirts

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -2,7 +2,7 @@ Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '96x96>', # thumbs under image
     :small => '200x200>', # images on category view
-    :product => '480x480>', # full product image
+    :product => '880x880>', # full product image
     :large => '1200x1200>' # light box image
   }
 

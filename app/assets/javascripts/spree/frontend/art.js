3f1a0e7469126144f7d365ad511b5c0bee2e0bac
dhiraj28691 <15dhirajmehta@gmail.com>
2017-11-19 12:01:18 +0530

    Adding Roads section
    Adding Roads section

--- app/assets/javascripts/spree/frontend/art.js
+++ app/assets/javascripts/spree/frontend/art.js
@@ -5,13 +5,14 @@ function Art() {
 
   "use strict";
 
-  var flkty, thumbnails_wrapper, flkty_tank,thumbnails_wrapper_tank,flkty_chair,thumbnails_wrapper_chair;
+  var flkty, thumbnails_wrapper, flkty_tank,thumbnails_wrapper_tank,flkty_chair,flkty_road,thumbnails_wrapper_chair,thumbnails_wrapper_road;
 
   Art.prototype.index = function() {
 
     thumbnails_wrapper = document.querySelector('.thumbnails');
     thumbnails_wrapper_tank = document.querySelector('.thumbnails.tank');
     thumbnails_wrapper_chair = document.querySelector('.thumbnails.chair');
+	thumbnails_wrapper_road = document.querySelector('.thumbnails.road');
 
 
 
@@ -67,11 +68,28 @@ function Art() {
       thumbnails_wrapper_chair.querySelector('.current').classList.remove('current');
       thumbnails_wrapper_chair.querySelectorAll('.thumbnail.chair').item(flkty_chair.selectedIndex).classList.add('current');
     });
+	
+	          flkty_road = new Flickity('#artbook-road', {
+      cellAlign: 'center',
+      cellSelector: '.slide',
+      contain: true,
+      imagesLoaded: true,
+      initialIndex: 2,
+      pageDots: false,
+      prevNextButtons: false,
+      wrapAround: true
+    });
+
+    flkty_chair.on( 'cellSelect', function() {
+      thumbnails_wrapper_road.querySelector('.current').classList.remove('current');
+      thumbnails_wrapper_road.querySelectorAll('.thumbnail.road').item(flkty_chair.selectedIndex).classList.add('current');
+    });
 
     // Slideshow Thumbnails
     bind_thumbnail_events('.thumbnails');
     bind_thumbnail_events_tank('.thumbnails');
 	bind_thumbnail_events_chair('.thumbnails');
+	bind_thumbnail_events_road('.thumbnails');
   }
   // Private functions
   function init_hero_video(selector) {
@@ -177,6 +195,22 @@ function Art() {
     document.querySelector('.thumbnails.chair').addEventListener('click', function(event){
       event.preventDefault();
       flkty_chair.select($(event.target.parentNode).index());
+    }, true);
+
+  }
+      function bind_thumbnail_events_road(selector) {
+
+    document.querySelector('.left.capture.road').addEventListener('click', function(event){
+      flkty_road.previous();
+    });
+
+    document.querySelector('.right.capture.road').addEventListener('click', function(event){
+      flkty_road.next();
+    });
+
+    document.querySelector('.thumbnails.road').addEventListener('click', function(event){
+      event.preventDefault();
+      flkty_road.select($(event.target.parentNode).index());
     }, true);
 
   }
